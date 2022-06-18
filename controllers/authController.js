f13c13ca6049dbcecba472d4c9b962be665f8c1f
CrackerJaps <thecrackerjaps@gmail.com>
2022-06-17 18:13:33 -0700

    login backend functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError } from '../errors/index.js'
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js'
 
 const register = async (req, res) => {
   const { name, email, password } = req.body
@@ -29,7 +29,23 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-  res.send('login user')
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+
+  const user = await User.findOne({ email }).select('+password')
+  if (!user) {
+    throw new UnauthenticatedError('Invalid credentials')
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Incorrect Password')
+  }
+  user.password = undefined
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
 
 const updateUser = async (req, res) => {
