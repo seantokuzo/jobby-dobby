d69829098f1b956d82b2ba9997e2513f9b004663
CrackerJaps <thecrackerjaps@gmail.com>
2022-06-23 17:27:30 -0700

    client - add job functionality

--- models/Job.js
+++ models/Job.js
@@ -14,7 +14,7 @@ const JobSchema = new mongoose.Schema(
     },
     status: {
       type: String,
-      enum: ['interview', 'decline', 'pending'],
+      enum: ['interview', 'declined', 'pending'],
       default: 'pending'
     },
     jobType: {
