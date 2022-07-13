0be04a73409ca7ab9769a5a430e8963267773175
seantokuzo <seantokuzo@gmail.com>
2022-07-13 11:04:17 -0700

    server - jobs pagination

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -53,11 +53,19 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position')
   }
 
+  // PAGINATION
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+
   const jobs = await result
 
-  res
-    .status(StatusCodes.OK)
-    .json({ totalJobs: jobs.length, jobs, numOfPages: 1 })
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalJobs / limit)
+
+  res.status(StatusCodes.OK).json({ totalJobs, numOfPages, jobs })
 }
 
 const updateJob = async (req, res) => {
