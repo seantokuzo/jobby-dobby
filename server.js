aea4668d9a2b63ee175f8fbdf1697752f1043470
seantokuzo <seantokuzo@gmail.com>
2022-07-14 12:03:28 -0700

    add security packages

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ dotenv.config()
 import 'express-async-errors'
 import morgan from 'morgan'
 
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+// SECURITY PACKAGES
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
+
 // DB AND AUTHENTICATE USER
 import connectDB from './db/connect.js'
 
@@ -20,7 +29,16 @@ import authenticateUser from './middleware/auth.js'
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
 }
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use(express.json())
+
+// SECURITY PACKAGES
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome' })
@@ -32,6 +50,10 @@ app.get('/api/v1', (req, res) => {
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './client/build/index.html'))
+// })
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
